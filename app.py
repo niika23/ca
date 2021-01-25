ee8483e9c00270df9df4cca5d2c6e29b276b0847
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-01-26 00:31:48 +0100

    Ahoj nika

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+
+import requests
+
+from ca import CA_1d
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
-    return render_template("main.html")
- 
-app.run(host='localhost', port=5000)
- 
+    graph_url = None
+    return render_template("main.html", graph_url=graph_url)
+
+@app.route('/a')
+def after_gen():
+    graph_url = "static/img/generated_ca.png"
+    print(graph_url)
+    return render_template("main.html", graph_url=graph_url)
+
+
+@app.route("/generator1d", methods=["POST", "GET"])
+def generator1D():
+    if request.method == "GET":
+        rule = request.args.get("rule")
+        col = request.args.get("col")
+
+        generator = CA_1d(int(rule), int(col))
+
+        return redirect(url_for("after_gen"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
