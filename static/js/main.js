718542320faad94019e6d0932f5beab403382b24
Martin Jenigár <martin.jenigar@student.tuke.sk>
2021-01-26 19:17:10 +0100

    ajax request

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,31 @@
-function back_to_main()
-{
-    window.location.replace("/");
-    console.log("aaaaa");
+$( document ).ready(function() {
+
+    $('#generate-btn').click(function(){
+        generateGraph();
+	});
+})
+
+
+function generateGraph()
+{   
+    // location.reload();
+    var rule = $('#rule-input').val();
+    var col = $('#col-input').val();
+
+    req_url = "/generator/"+rule+"/"+col;
+    console.log(req_url);
+
+    $.ajax({
+        url: req_url,
+        data: {data: 1},
+        type: 'POST',
+        success: function(response){
+            data = JSON.parse(response);
+            console.log(data.url);
+            $("#generated-ca-img").attr('src', data.url);
+        },
+        error: function(error){
+            console.log("Something went ");
+        }
+    });
 }
